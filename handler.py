0afe20d6e32a78d46f7630c9877a86bdd61309d8
Mark Hunt <thefreehunter@gmail.com>
2020-10-26 21:48:38 +0000

    adding right font, UUIDs for names, and returning filename

--- handler.py
+++ handler.py
@@ -1,50 +1,27 @@
+# From the Lambda layer
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
+# AWS imports
 import boto3
-import base64
 from boto3 import client
 
+# Python standard library imports
 import os
 import io
-
-# def meme(event, context):
-#     user_download_img ='meme.jpg'
-#     print('user_download_img ==> ',user_download_img)
-
-#     s3 = boto3.resource('s3')
-#     bucket = s3.Bucket(u'memendous') 
-#     obj = bucket.Object(key=user_download_img)      #pass your image Name to key
-    
-#     response = obj.get()     #get Response
-#     img = response[u'Body'].read()        # Read the respone, you can also print it.
-#     print(type(img))                      # Just getting type.
-#     myObj = [base64.b64encode(img)]          # Encoded the image to base64
-
-#     print(type(myObj))            # Printing the values
-#     print(myObj[0])               # get the base64 format of the image
-#     print('type(myObj[0]) ================>',type(myObj[0]))
-
-#     return_json = str(myObj[0])           # Assing to return_json variable to return.  
-#     print('return_json ========================>',return_json)
-
-#     return_json1 = return_json.replace("b'","")          # repplace this 'b'' is must to get absoulate image.
-#     encoded_image = return_json1.replace("'","")   
-
-#     s3 = boto3.resource('s3')
-#     object = s3.Object('my_bucket_name', 'my/key/including/filename.txt')
-#     object.put(Body=some_binary_data)
-
-
-
-
+import uuid
+import base64
+import json
 
 def meme(event, context):
-    # bucketname = 'memendous' # replace with your bucket name
-    # filename = 'background.png' # replace with your object key
-    # s3 = boto3.resource('s3')
-    # s3.Bucket(bucketname).download_file(filename, '/tmp/meme.png')
+    
+    #TODO
+    # 1. Read images from S3
+    # 2. Put text in correct place
+    # 3. Center/wrap text if needed (https://www.haptik.ai/tech/putting-text-on-images-using-python-part2/)
+    # 4. Make UI in JS or Vue
+    # 5. Deploy UI to Amplify
     
     base_path = os.environ['IMAGES_DIR']
     
@@ -58,30 +35,32 @@ def meme(event, context):
     txt = Image.new('RGBA', base.size, (255,255,255,0))
 
     # get a font
-    # fnt = ImageFont.truetype('Arial', 40)
+    fnt = ImageFont.truetype(f'{base_path}font/Roboto-Bold.ttf', size=45)
+    #fnt = ImageFont.truetype(f'{base_path}font/Roboto-Black.ttf', 40)
     # get a drawing context
     d = ImageDraw.Draw(txt)
 
     # draw text, half opacity
-    d.text((10,10), "Hello", fill=(255,255,255,128))
+    d.text((10,10), "Hello", font=fnt, fill=(255,255,255,128))
     # draw text, full opacity
-    d.text((10,60), "World", fill=(255,255,255,255))
+    d.text((10,60), "Roboto", font=fnt, fill=(255,255,255,255))
 
     out = Image.alpha_composite(base, txt)
+    
+    # Generate a UUID for the name
+    meme_name = uuid.uuid4()
 
-    out.save('/tmp/greeting_card.png')
+    out.save(f'/tmp/{meme_name}.png')
     
     print("made the image, now to upload it")
     
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.upload_file('/tmp/greeting_card.png', 'memendous', 'greeting_card.png')
+        response = s3_client.upload_file(f'/tmp/{meme_name}.png', 'memendous', f'{meme_name}.png')
     except:
         print("error")
         return False
-    return True
-    
-    # object = s3.Object('memendous', '/tmp/greeting_card.png')
-    # object.put(Body=out)
-    
-    return out
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f'https://memendous.s3.us-east-2.amazonaws.com/{meme_name}.png')
+    }
